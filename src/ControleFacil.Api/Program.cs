949a7afdd2612cd04dce88c57392946cf796a1cf
Tavilo Breno - PC4 <breno_wk2@hotmail.com>
2023-11-07 00:35:21 -0300

    Configurando o projeto

--- src/ControleFacil.Api/Program.cs
+++ src/ControleFacil.Api/Program.cs
@@ -1,25 +1,111 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigurarServices(builder);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigurarInjecaoDeDependencia(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigurarAplicacao(app);
 
 app.Run();
+
+// Metodo que configrua as injeções de dependencia do projeto.
+static void ConfigurarInjecaoDeDependencia(WebApplicationBuilder builder)
+{
+    builder.Services
+    .AddSingleton(builder.Configuration)
+    .AddSingleton(builder.Environment);
+}
+
+// Configura o serviços da API.
+static void ConfigurarServices(WebApplicationBuilder builder)
+{
+    builder.Services
+    .AddCors()
+    .AddControllers().ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    }).AddNewtonsoftJson();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JTW Authorization header using the Beaerer scheme (Example: 'Bearer 12345abcdef')",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "ControleFacil.Api", Version = "v1" });   
+    });
+
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["KeySecret"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+// Configura os serviços na aplicação.
+static void ConfigurarAplicacao(WebApplication app)
+{
+    // Configura o contexto do postgreSql para usar timestamp sem time zone.
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    app.UseDeveloperExceptionPage()
+        .UseRouting();
+
+    app.UseSwagger()
+        .UseSwaggerUI(c =>
+        {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ControleFacil.Api v1");
+                c.RoutePrefix = string.Empty;
+        });
+
+    app.UseCors(x => x
+        .AllowAnyOrigin() // Permite todas as origens
+        .AllowAnyMethod() // Permite todos os métodos
+        .AllowAnyHeader()) // Permite todos os cabeçalhos
+        .UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+    app.MapControllers();
+}
